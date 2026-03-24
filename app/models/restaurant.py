fc3db014cbcd05718b00c700a707319fe9ecdd14
María Báez <baezmarreromaria9@gamil.com>
2026-03-24 21:12:15 +0000

    Initial commit

---
+++ app/models/restaurant.py
@@ -0,0 +1,9 @@
+class Restaurant:
+    def __init__(self, name, rating, location, distance, cuisine_type, telephone=None):
+        self.name = name
+        self.rating = rating
+        self.location = location
+        self.distance = distance
+        self.cuisine_type = cuisine_type
+        self.telephone = telephone
+
---
+++ app/services/restaurant_api_client.py
@@ -0,0 +1,7 @@
+api_key = ZZSTO04KMZDL5HPL2DQKQNJBT1LOXUKASMIJNKJ5KPNLLY5F
+
+class RestaurantApiClient:
+    def __init__(self, api_key, base_url):
+        self.base_url = base_url
+        self.api_key = api_key
+
