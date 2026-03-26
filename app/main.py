5eb4376276d30e72a65d918ea0063e3b603a82cf
María Báez <baezmarreromaria9@gmail.com>
2026-03-26 17:31:33 +0000

    Functioning scraper with basic web

--- app/main.py
+++ app/main.py
@@ -1,21 +1,18 @@
 from app.services.restaurant_search_service import restaurantSearchService
 from app.services.favorites_manager import FavoritesManager
 from app.models.restaurant import Restaurant
+from app.services.restaurant_scrapper import RestaurantScrapper
 
-service = restaurantSearchService("Madrid", "italian")
+#service = restaurantSearchService("Madrid", "italian")
 
-response = service.transform_response(service.search())
+#response = service.transform_response(service.search())
 
-response = service.order_by_distance(response)
+#response = service.order_by_distance(response)
 
 
+scrap = RestaurantScrapper()
 
-favorites = FavoritesManager()
-
-favorites.add_favorite(response[0])
-favorites.save_favorites()
-
-favorites.load_favorites()
-
-for fav in favorites.favorites:
-    print(fav)
+html = scrap.get_page("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+soup = scrap.parse_page(html)
+soup = scrap.extract_rating_count(soup)
+print(soup)
--- app/services/restaurant_scrapper.py
+++ app/services/restaurant_scrapper.py
@@ -1,14 +1,19 @@
 import requests
-import BeautifulSoup
+from bs4 import BeautifulSoup
 class RestaurantScrapper:
     def __init__(self):
         pass
 
     def get_page(self, url):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        }
+
         if not url:
             return "error"
         try:
-            response = requests.get(url)
+            response = requests.get(url, headers=headers, timeout=10)
+            response.raise_for_status()
             if response.status_code != 200:
                 print(f"Error fetching page: {response.status_code}")
                 return "error"
@@ -27,11 +32,21 @@ class RestaurantScrapper:
     def extract_rating_count(self, soup):
         if not soup:
             return "error"
-        rating_tag = soup.find("div", {"data-testid": "BizHeaderReviewCount"})
+
+        rating_tag = soup.find("p", class_="star-rating")
         if not rating_tag:
             print("Rating tag not found")
             return "N/A"
-        rating_text = rating_tag.text.strip()
-        rating_text = rating_text[0:rating_text.find(" ")]
-        rating_int = int(rating_text.replace(",", ""))
-        return rating_int
+
+        rating_class = rating_tag["class"][1]
+
+        mapping = {
+            "One": 1,
+            "Two": 2,
+            "Three": 3,
+            "Four": 4,
+            "Five": 5
+        }
+
+        rating = mapping.get(rating_class, "N/A")
+        return rating
